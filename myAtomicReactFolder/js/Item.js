939c6ec31ac1fdbf9754fa5d04ef872d2effd1dd
Guihgo <guihgo100milha@gmail.com>
2018-12-06 02:26:38 -0200

    migrating to AtomicReact 1.1.15

--- myAtomicReactFolder/js/Item.js
+++ myAtomicReactFolder/js/Item.js
@@ -1,19 +1,23 @@
-module.exports.onRender = function(atom){
-  let btnAction = Atomic.getSub(atom, 'btnAction');
-  let name = Atomic.getSub(atom, 'name');
+class Main {
+  onRender(atom) {
+    let btnAction = Atomic.getSub(atom, 'btnAction');
+    let name = Atomic.getSub(atom, 'name');
 
-  let markAsDone = "Mark as done";
-  let markAsUnDone = "Mark as undone";
+    let markAsDone = "Mark as done";
+    let markAsUnDone = "Mark as undone";
 
-  btnAction.innerHTML = markAsDone;
+    btnAction.innerHTML = markAsDone;
 
-  btnAction.onclick = function(){
-    if(name.classList.contains('done')) {
-      name.classList.remove('done');
-      btnAction.innerHTML = markAsDone;
-    } else {
-      name.classList.add('done');
-      btnAction.innerHTML = markAsUnDone;
+    btnAction.onclick = function(){
+      if(name.classList.contains('done')) {
+        name.classList.remove('done');
+        btnAction.innerHTML = markAsDone;
+      } else {
+        name.classList.add('done');
+        btnAction.innerHTML = markAsUnDone;
+      }
     }
   }
-};
+}
+
+module.exports.main = Main;
